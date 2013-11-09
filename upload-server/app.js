f1f7546469b18eaeeb1b871a77674e566d2fdb1f
Hank Wang <drapho@gmail.com>
2013-11-09 14:07:53 +0800

    mv template to main-server

--- upload-server/app.js
+++ upload-server/app.js
@@ -41,6 +41,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.post('/record', routes.addRecord);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- upload-server/routes/index.js
+++ upload-server/routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.addRecord = function(req, res) {
+    console.log(req.session);
+};
