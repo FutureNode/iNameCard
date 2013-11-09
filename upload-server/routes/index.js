6f16ac0d6d5bb9e875b6b2dc1bbdeea787d4c7a1
Hank Wang <drapho@gmail.com>
2013-11-09 18:12:04 +0800

    use ffmpeg to convert mp3

--- upload-server/routes/index.js
+++ upload-server/routes/index.js
@@ -12,20 +12,34 @@ exports.addRecord = function(req, res) {
     var fs = require('fs');
     var path = require('path');
     var filename = 'test.wav'
-    console.log(fs.existsSync(req.files.file.path));
-    console.log(path.join(__dirname, '..', 'trackbase', filename));
-    fs.rename(req.files.file.path, path.join(__dirname, '../../', 'trackbase', filename), function(err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('ok');
-            // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
-            // fs.unlink(tmp_path, function() {
-            //     if (err) throw err;
-            //     res.send('File uploaded');
-            // });
+
+    // console.log(path.join(__dirname, '..', 'trackbase', filename));
+    var exec = require('child_process').exec,
+    child;
+    var srcAudio = req.files.file.path;
+    var targetAudio = path.join(__dirname, '../../', 'trackbase', 'test.mp3');
+
+    child = exec('ffmpeg -i ' + srcAudio + ' -f mp3 ' + targetAudio, function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+          console.log('exec error: ' + error);
         }
     });
+
+
+    // fs.rename(req.files.file.path, path.join(__dirname, '../../', 'trackbase', filename), function(err) {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         console.log('ok');
+    //         // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+    //         // fs.unlink(tmp_path, function() {
+    //         //     if (err) throw err;
+    //         //     res.send('File uploaded');
+    //         // });
+    //     }
+    // });
 
   //   mv(req.files.file.path, '../trackbase', function(err) {
   //       if (err) console.log(err);
