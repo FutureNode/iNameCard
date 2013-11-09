8b583e4c43e9b2569acfd244918d12f8d51c841e
Hank Wang <drapho@gmail.com>
2013-11-09 16:04:48 +0800

    fixed

--- upload-server/routes/index.js
+++ upload-server/routes/index.js
@@ -2,12 +2,13 @@
 /*
  * GET home page.
  */
+var file = require('file');
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
 exports.addRecord = function(req, res) {
-    res.json(req.body.record);
-    console.log(req.session);
+    res.json(req.files);
+    // console.log(req.session);
 };
