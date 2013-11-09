d406fc9032c02547a1cf5a674b0f9a4ce4bb8c40
Hank Wang <drapho@gmail.com>
2013-11-09 16:07:11 +0800

    enabled bodyParser

--- upload-server/app.js
+++ upload-server/app.js
@@ -25,7 +25,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.bodyParser());
+app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser());
 app.use(express.session({
