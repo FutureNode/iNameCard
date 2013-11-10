473d62e5593cd1c5fed8c8614661b2688d191f29
Hank Wang <drapho@gmail.com>
2013-11-10 13:08:22 +0800

    fix bug

--- upload-server/routes/index.js
+++ upload-server/routes/index.js
@@ -91,9 +91,14 @@ exports.addMusic = function(req, res) {
     // var user = req.session.passport ? req.session.passport.user : '';
     var programId = req.body.programId;
     var musicId = req.body.musicId;
-    if (!programId || !musicId) res.json({error: 'program Id not found'});
+    if (!programId || !musicId) return res.json({error: 'program Id not found'});
+
+
+    console.log(programId);
+    console.log(musicId);
 
     req.app.db.models.Program.update({_id: programId}, {music: musicId}, { upsert: false, multi: true }, function(err, numAffected) {
+        console.log(ok);
         res.json({status: 'ok'});
     });
 };
