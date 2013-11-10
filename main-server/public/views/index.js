bbbf77f3ebafba23cb7feb91dfbe1c13eb6114e3
root <root@localhost.(none)>
2013-11-10 02:42:46 +0000

    change style

--- main-server/public/views/index.js
+++ main-server/public/views/index.js
@@ -1,3 +1,28 @@
-$.get('/program', function(programs) {
+function appendItem(program) {
+
+	if (!program.image)
+		return;
+
+	var $item = $('<div>').addClass('col-md-3');
+	var $thumbnail = $('<div>').addClass('thumbnail');
+	var $img = $('<img>')
+		.addClass('img-rounded')
+		.attr('src', '/track/' + program.image);
+	var $caption = $('<div>').addClass('caption');
+
+	$thumbnail.append($img);
+	$item.append($thumbnail);
+
+	$('#album').append($item);
+
+}
+
+$.get('/program', function(data) {
 	console.log(programs);
+
+	var programs = data.programs;
+
+	for (var index in programs) {
+		appendItem(programs[index]);
+	}
 });
--- stream-server/app.js
+++ stream-server/app.js
@@ -5,6 +5,8 @@ var url = require('url');
 var fs = require('fs');
 var util = require('util');
 var path = require('path');
+var async = require('async');
+var mime = require('mime');
 
 var trackbasePath = path.join(__dirname, '..', 'trackbase');
  
@@ -13,66 +15,103 @@ http.createServer(function(req, res) {
 	var urlSet = url.parse(req.url);
 
 	var trackID = path.basename(urlSet.pathname);
-	var trackPath = path.join(trackbasePath, trackID + '.mp3');
 
-	// Check the specific file
-	fs.exists(trackPath, function(exists) {
-		if (!exists) {
-			res.statusCode = 404;
-			res.end('NOT FOUND');
-			return;
-		}
+	var list = [
+		path.join(trackbasePath, trackID + '.jpg'),
+		path.join(trackbasePath, trackID + '.png'),
+		path.join(trackbasePath, trackID + '.mp3')
+	];
 
-		// Getting file status
-		fs.stat(trackPath, function(err, stats) {
+	var trackPath = null
 
-			if (err) {
-				res.end('ERROR');
-				res.statusCode = 500;
-				res.end();
-				return;
+	async.series([
+		function(next) {
+			async.eachSeries(list, function(item, _next) {
+
+				// Check the specific file
+				fs.exists(item, function(exists) {
+					if (exists) {
+						_next(item);
+						return;
+					}
+
+					_next();
+				});
+			}, function(item) {
+				trackPath = item;
+
+				next();
+			});
+		},
+		function(next) {
+			if (!trackPath) {
+				next(false);
 			}
 
-			if (!stats.size) {
-				res.statusCode = 404;
-				res.end('NOT FOUND');
-				return;
-			}
+			next();
+		},
+		function(next) {
+			// Getting file status
+			fs.stat(trackPath, function(err, stats) {
 
-			// Request range of file
-			if (req.headers['range']) {
+				if (err) {
+					res.end('ERROR');
+					res.statusCode = 500;
+					res.end();
+					return;
+				}
 
-				// Getting range and chunk size
-				var range = req.headers.range;
-				var parts = range.replace(/bytes=/, "").split("-");
-				var start = parseInt(parts[0], 10);
+				if (!stats.size) {
+					res.statusCode = 404;
+					res.end('NOT FOUND');
+					return;
+				}
 
-				var end = parts[1] ? parseInt(parts[1], 10) : stats.size - 1;
-				var chunkSize = (end - start) + 1;
+				// Request range of file
+				if (req.headers['range']) {
 
-				// Ready to go
-				res.writeHead(206, {
-					'Content-Range': 'bytes ' + start + '-' + end + '/' + stats.size,
-					'Accept-Ranges': 'bytes',
-					'Content-Length': chunkSize,
-					'Content-Type': 'audio/mpeg'
+					// Getting range and chunk size
+					var range = req.headers.range;
+					var parts = range.replace(/bytes=/, "").split("-");
+					var start = parseInt(parts[0], 10);
+
+					var end = parts[1] ? parseInt(parts[1], 10) : stats.size - 1;
+					var chunkSize = (end - start) + 1;
+
+					// Ready to go
+					res.writeHead(206, {
+						'Content-Range': 'bytes ' + start + '-' + end + '/' + stats.size,
+						'Accept-Ranges': 'bytes',
+						'Content-Length': chunkSize,
+						'Content-Type': mime.lookup(trackPath)
+					});
+
+					var source = fs.createReadStream(trackPath, { start: start, end: end });
+					source.pipe(res);
+
+					return;
+				}
+
+				// Send out whole file
+				res.writeHead(200, {
+					'Content-Length': stats.size,
+					'Content-Type': mime.lookup(trackPath)
 				});
 
-				var source = fs.createReadStream(trackPath, { start: start, end: end });
-				source.pipe(res);
+				fs.createReadStream(trackPath).pipe(res);
 
-				return;
-			}
-
-			// Send out whole file
-			res.writeHead(200, {
-				'Content-Length': stats.size,
-				'Content-Type': 'audio/mpeg'
 			});
 
-			fs.createReadStream(trackPath).pipe(res);
+			next(true);
 
-		});
+		}
+	], function(found) {
+
+		if (!found) {
+			res.statusCode = 404;
+			res.end('NOT FOUND');
+		}
 	});
+
 
 }).listen(8200);
